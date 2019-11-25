24968aa8902aaf6119a0603d33d694811b4c04df
Parvathyrb <58171746+Parvathyrb@users.noreply.github.com>
2019-11-25 23:39:35 +0530

    sum of n numbers

---
+++ Summ.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Summ {
+public static void main(String[]args)
+{
+	int sum=0;
+	int a[]= {10,20,50,40};
+
+	System.out.println("Enter the numbers");
+	for (int i=0;i<3;i++)
+	{
+		sum=sum+a[3];
+	}
+	System.out.println("sum:"+sum);
+
+}
+}
+
+
+
